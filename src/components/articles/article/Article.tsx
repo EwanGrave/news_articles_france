7cdd765f7678b38fc5f33131625296ea1f0089b1
Ewan Gravé <grave.ewan2001@gmail.com>
2025-04-07 16:31:15 +0200

    feat: article style

--- src/components/articles/article/Article.tsx
+++ src/components/articles/article/Article.tsx
@@ -1,11 +1,30 @@
 import React, { JSX } from "react";
 import "./Article.css";
 import { ArticleType } from "../../../types/ArticleType";
+import { Card } from "antd";
+import Meta from "antd/es/card/Meta";
 
 interface ArticleProps {
   article: ArticleType;
 }
 
 export default function Article(props: ArticleProps): JSX.Element {
-  return <div>{props.article.title}</div>;
+  return (
+    <div className="article">
+      <Card
+        className="article-card"
+        style={{ width: 300 }}
+        cover={<img alt="example" src={props.article.urlToImage} />}
+      >
+        <Meta
+          title={
+            <a href={props.article.url} target="_BLANK">
+              {props.article.title}
+            </a>
+          }
+          description={props.article.description}
+        />
+      </Card>
+    </div>
+  );
 }
